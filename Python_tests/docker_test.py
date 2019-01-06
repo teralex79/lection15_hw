d3da00c633675c56b6f94e255465a0537c1715c3
Aleksei Terentev <teralex79@gmail.com>
2019-01-06 18:53:28 +0300

    Script with all needed functions

--- Python_tests/docker_test.py
+++ Python_tests/docker_test.py
@@ -2,9 +2,13 @@
 
 import requests
 import json
-from colorama import Fore, Back, Style, init
+#from colorama import Fore, Back, Style, init
 
-init(autoreset=True)
+white = '\033[' + str(0) + 'm'
+red = '\033[' + str(31) + 'm'
+green = '\033[' + str(32) + 'm'
+yellow = '\033[' + str(33) + 'm'
+grey = '\033[' + str(90) + 'm'
 
 url = 'http://192.168.56.103:2376/'
 
@@ -20,33 +24,61 @@ for item in tupl1:
     for stt in filtr_tupl:
       fltr = json.dumps({'status': [stt]})
       get_str = url + item + addstr1 + addstr2 + fltr
-      print(get_str)
       r = requests.get(get_str)
       r_json = json.loads(r.text)
       n = len(r_json)
-      print(r_json)
+      clr = grey
+      if len(r_json) > 0:
+        clr = yellow
+        if stt == 'running':
+          clr = green
+        elif stt == 'dead':
+          clr = red
+
       w1 = item
       if n == 1:
-         w1 = 'container'
+        w1 = 'container'
       elif n == 0:
-         n = 'no'
+        n = 'no'
       msg = ''
-      print(msg + 'You have {0} {1} {2}.'.format(n, stt, w1))
-#      for nm in head_dic[item]:
-#        str1 += 
-#  else: 
-
-
-
-#str1 = url + tupl1[0] + addstr 
-#+ str(&filters=\{status\:\[filtr_tupl[0]\]\})
-#print(str1)
-#r_con = requests.get(url + tupl1[0] + addstr + str(&filters=\{status\:\[filtr_tupl[0]\]\}))
-#r_con_json = json.loads(r_con.text)
-#r_img = requests.get(url + '/images/json?all=1')
-#r_img_json = json.loads(r_img.text)
-
-#print(r_con_json)
-#color_text = r_con_json[0]['Names']
-#print(Fore.RED + str(color_text))
-#print(color_text)
+ 
+      print(clr + 'You have {0} \"{1}\" {2}.'.format(n, stt, w1) + white)
+      hd_str = ''
+      for nm in head_dic[item]:
+        hd_str += '{0:<15s}'.format(nm.upper())
+      if len(r_json) > 0:
+        print(hd_str)
+        for cont in r_json:
+          cont_str = '' 
+          for nm in head_dic[item]:
+            if nm == 'Id': 
+              cont[nm] = cont[nm][:12]
+            elif nm == 'Names':
+              cont[nm] = cont[nm][0][1:]  
+            cont_str += '{0:<15s}'.format(cont[nm])
+          print(cont_str)
+  else:
+    get_str = url + item + addstr1 + addstr2
+    r = requests.get(get_str)
+    r_json = json.loads(r.text)
+    n = len(r_json)
+    w1 = item
+    if n == 1:
+      w1 = 'image'
+    elif n == 0:
+      n = 'no'
+    print(yellow + 'You have {0} {1}.'.format(n, w1) + white)
+    hd_str = ''
+    for nm in head_dic[item]:
+      if nm == 'RepoTags': nm = nm[4:-1]
+      hd_str += '{0:<15s}'.format(nm.upper())
+    if len(r_json) > 0:
+      print(hd_str)
+      for img in r_json:
+        img_str = ''
+        for nm in head_dic[item]:
+          if nm == 'Id': 
+             img[nm] = img[nm].split(':')[1][:12]
+          img_str += '{0:>15s}'.format(str(img[nm]))
+        print(img_str)
+ 
